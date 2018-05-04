11549356c114e7db4603b6fd8f70a8f8bb86b9fa
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-03 21:05:50 -0300

    Add more info in "cursos", "frontend" and "backend"

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,19 +1,27 @@
 export const languages = {
   html5: {
     title: 'HTML5',
-    description: 'Conhecimento de 95% da linguagem de marcação, conhecendo tanto o básico como o avançado como SEO e acessibilidade.'
+    description: 'Conhecimento de 95% da linguagem de marcação, conhecendo tanto o básico como o avançado como SEO, Acessibilidade e LocalStorage/SessionStorage. Conhecimento básico sobre as APIs de canvas, video e audio, sendo os restantes para o entendimento completo da linguagem.'
   },
   css3: {
     title: 'CSS3',
-    description: 'Conhecimento de 90% da linguagem, utilização das novas funcionalidades do CSS3 bem sólidas.'
+    description: 'Conhecimento de 90% da linguagem, utilização das novas funcionalidades do CSS3 bem sólidas como Animações, Flexbox, etc, faltando somente propriedades mais especificas e pouco utilizadas.'
   },
   sass: {
     title: 'Sass',
-    description: 'Conhecimento de 50% do pré processador, codificação é baseada em padrões de styleguide presentes em https://sass-guidelin.es'
+    description: 'Conhecimento de 60% do pré processador, codificação seguindo padrões de styleguide presentes em https://sass-guidelin.es .'
   },
   javascript: {
     title: 'Javascript',
-    description: 'Conhecimentos sólidos da linguagem até o ES6, utilizando padrões de projeto e organização de código utilizando o Webpack e afins.'
+    description: 'Conhecimentos sólidos da linguagem até o ES6 com algumas funcionalidades do ES7/ES8. Utilização de padrões de projeto e organização de código utilizando o Webpack e afins.'
+  },
+  framework_css: {
+    title: 'Frameworks CSS',
+    description: 'Conhecimento sobre os frameworks CSS mais famosos do mercado como Bootstrap, Materialize e Bulma'
+  },
+  spa: {
+    title: 'Single Page Application',
+    description: 'Conhecimento básico sobre os demais SPAs do mercado atual como Angular, Vue e React. Entendimento da funcionalidade de suas bibliotecas externas e qual problemas elas resolvem (Vuex/Redux, Next/Nuxt, etc). Dentre eles o mais conhecido sendo o Angular por iniciar a codificação da estrutura de uma rede social.'
   },
   php: {
     title: 'PHP',
@@ -25,10 +33,18 @@ export const languages = {
   },
   wordpress: {
     title: 'Wordpress',
-    description: 'Trabalhei durante 1 ano com este framework, conhecendo assim suas peculiariedades e sistema de codificação.'
+    description: 'Trabalhado durante 1 ano com este framework, conhecendo assim suas peculiariedades e sistema de codificação.'
   },
   magento: {
     title: 'Magento',
     description: 'Framework completo para criação de sistemas de e-commerce complexos, conhecimentos para alterações de temas e criação de pequenos módulos para integrar ao sistema.'
+  },
+  git: {
+    title: 'Git',
+    description: 'Conhecimento básico sobre as funcionalidades mais utilizadas durante o dia a dia.'
+  },
+  rest: {
+    title: 'REST',
+    description: 'Conhecimento sólido sobre utilização e criação de APIs REST.'
   }
 };
